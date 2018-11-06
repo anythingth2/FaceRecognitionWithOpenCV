30cec7212616d171b58032b3ea2c2cf4c40785fa
anythingth2 <chatchaishaetan@gmail.com>
2018-11-06 14:33:35 +0000

    Implement open door api

--- StreamingServer.py
+++ StreamingServer.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template,request, Response
 import cv2
+import RPi.GPIO as GPIO
+from threading import Timer 
+import time
+
+
 cam = cv2.VideoCapture(0)
 app = Flask(__name__)
 
+doorPins = [2,3,4,17]
+isOpening = False
+
+GPIO.setmode(GPIO.BCM)
+    
+GPIO.setup(doorPins, GPIO.OUT,initial=GPIO.LOW)
+#GPIO.cleanup()
 
 def generateFrame():
     while cam.isOpened():
@@ -20,10 +32,22 @@ def index():
 @app.route('/vid')
 def video_feed():
     return Response(generateFrame(),mimetype='multipart/x-mixed-replace; boundary=frame')
+def openDoor(index):
+    
+    global isOpening
+    if isOpening:
+        return
+    isOpening = True
+    GPIO.output(doorPins[index],GPIO.HIGH)
+    time.sleep(0.1)
+    GPIO.output(doorPins[index],GPIO.LOW)
+    time.sleep(5)
+    print('open {}'.format((index)))
+    isOpening = False
 @app.route('/open',methods=['GET'])
-def openDoor():
+def openDoorApi():
     doorIndex = request.args.get('door',type = int)
-    print('open at {}'.format(doorIndex))
+    openDoor(doorIndex)
     return Response()
 @app.route('/image')
 def image():
@@ -36,6 +60,7 @@ def image():
     return buff.tobytes()
 
 if __name__ == '__main__':
+    
     app.run(host='0.0.0.0',threaded=True)
 
         
